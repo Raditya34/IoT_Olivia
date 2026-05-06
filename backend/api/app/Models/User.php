a175765473c9567264a331fa19ccb0ebf51ac62d
Raditya34 <difa082402@gmail.com>
2026-05-06 23:21:18 +0700

    token

--- backend/api/app/Models/User.php
+++ backend/api/app/Models/User.php
@@ -2,22 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens; //
 
 class User extends Authenticatable
 {
+    // Menggunakan trait HasApiTokens agar bisa generate token untuk mobile
     use HasApiTokens, HasFactory, Notifiable;
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * Nama field yang boleh diisi (mass assignable).
      */
     protected $fillable = [
         'name',
@@ -26,22 +22,23 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
+     * Field yang disembunyikan saat data dikirim ke Flutter.
      */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-public function devices()
-{
-    return $this->hasMany(\App\Models\Device::class);
-}
+
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Relasi ke tabel devices (jika kamu menggunakannya).
+     */
+    public function devices()
+    {
+        return $this->hasMany(\App\Models\Device::class);
+    }
+
+    /**
+     * Konfigurasi casting field database.
      */
     protected function casts(): array
     {
