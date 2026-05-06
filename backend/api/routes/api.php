f5d2900e5a29180b2181923dd07db32e2087cf08
Raditya34 <difa082402@gmail.com>
2026-05-06 23:55:45 +0700

    sss

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\OliviaController;
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\OliviaController;
 
 /*
 |--------------------------------------------------------------------------
